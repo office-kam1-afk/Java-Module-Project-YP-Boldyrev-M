eb389ba09bf196255f0da3ae7dcd0a03acdb6f33
nemo <office-kam1@yandex.ru>
2025-10-06 19:00:12 +0300

    программа Ралли

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,65 +1,36 @@
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        RacingCar[] racingCars = new RacingCar[3];
         Race race = new Race();
-        int speedCar;
+        final int minSpeed = 0;
+        final int maxSpeed = 250;
         for (int i = 0; i < 3; i++) {
-        System.out.println("- Введите название машины №" + (i + 1) + ":");
-        String racingName = scanner.nextLine();
-          while (true) {
-         System.out.println(" - Введите скорость машины №" + (i + 1) + ":");
-         if (scanner.hasNextInt()) {
+            System.out.println("- Введите название машины №" + (i + 1) + ":");
+            String racingName = scanner.nextLine();
+            int speedCar;
+            while (true) {
+                System.out.println("- Введите скорость машины №" + (i + 1) + ":");
+                if (scanner.hasNextInt()) {
                     speedCar = scanner.nextInt();
-                    if (speedCar >= 0 && speedCar <= 250) {
+                    if (speedCar >= minSpeed && speedCar <= maxSpeed) {
                         break;
                     } else {
-                        System.out.println(" - Неправильная скорость ");
+                        System.out.println("- Неправильная скорость");
                     }
                 } else {
+                    System.out.println(" - Введите целое число!");
                     scanner.next();
                 }
             }
             scanner.nextLine();
-            racingCars[i] = new RacingCar(racingName, speedCar);
-            race.addRaceCar(racingCars[i]);
+           race.addRaceCar(new RacingCar(racingName, speedCar));
         }
-
         System.out.println("Самая быстрая машина: " + race.getWinner().getRacingName());
+
         scanner.close();
-    }
-
-    static class RacingCar {
-        private String racingName;
-        private int speedCar;
-
-        RacingCar(String racingName, int speedCar) {
-            this.racingName = racingName;
-            this.speedCar = speedCar;
-        }
-
-        public String getRacingName() {
-            return racingName;
-        }
-
-        public int getSpeedCar() {
-            return speedCar;
-        }
-    }
-
-    static class Race {
-        private RacingCar currentRaceLeader = null;
-
-        public void addRaceCar(RacingCar raceCar) {
-            if (currentRaceLeader == null || raceCar.getSpeedCar() > currentRaceLeader.getSpeedCar()) {
-                currentRaceLeader = raceCar;
-            }
-        }
-
-        public RacingCar getWinner() {
-            return currentRaceLeader;
-        }
     }
 }
---
+++ src/main/java/RacingCar.java
@@ -0,0 +1,18 @@
+public class RacingCar {
+    private String racingName;
+    private int speedCar;
+
+    public RacingCar(String racingName, int speedCar) {
+        this.racingName = racingName;
+        this.speedCar = speedCar;
+    }
+
+    public String getRacingName() {
+        return racingName;
+    }
+
+    public int getSpeedCar() {
+        return speedCar;
+    }
+}
+
