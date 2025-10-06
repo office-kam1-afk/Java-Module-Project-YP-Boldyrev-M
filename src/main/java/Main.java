ee3c54f1a09451e8685326d9c28892d01a8d4f0e
nemo <office-kam1@yandex.ru>
2025-10-06 19:00:12 +0300

    программа Ралли

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,65 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        RacingCar[] racingCars = new RacingCar[3];
+        Race race = new Race();
+        int speedCar;
+        for (int i = 0; i < 3; i++) {
+        System.out.println("- Введите название машины №" + (i + 1) + ":");
+        String racingName = scanner.nextLine();
+          while (true) {
+         System.out.println(" - Введите скорость машины №" + (i + 1) + ":");
+         if (scanner.hasNextInt()) {
+                    speedCar = scanner.nextInt();
+                    if (speedCar >= 0 && speedCar <= 250) {
+                        break;
+                    } else {
+                        System.out.println(" - Неправильная скорость ");
+                    }
+                } else {
+                    scanner.next();
+                }
+            }
+            scanner.nextLine();
+            racingCars[i] = new RacingCar(racingName, speedCar);
+            race.addRaceCar(racingCars[i]);
+        }
+
+        System.out.println("Самая быстрая машина: " + race.getWinner().getRacingName());
+        scanner.close();
+    }
+
+    static class RacingCar {
+        private String racingName;
+        private int speedCar;
+
+        RacingCar(String racingName, int speedCar) {
+            this.racingName = racingName;
+            this.speedCar = speedCar;
+        }
+
+        public String getRacingName() {
+            return racingName;
+        }
+
+        public int getSpeedCar() {
+            return speedCar;
+        }
+    }
+
+    static class Race {
+        private RacingCar currentRaceLeader = null;
+
+        public void addRaceCar(RacingCar raceCar) {
+            if (currentRaceLeader == null || raceCar.getSpeedCar() > currentRaceLeader.getSpeedCar()) {
+                currentRaceLeader = raceCar;
+            }
+        }
+
+        public RacingCar getWinner() {
+            return currentRaceLeader;
+        }
     }
 }
